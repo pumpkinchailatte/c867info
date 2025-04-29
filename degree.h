2a16d97f80b73459be93a577e4c6ab7bd4d3b4e2
pumpkinchailatte <rachel.fleming611@gmail.com>
2025-04-29 07:41:29 -0600

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+/*ennumerated data type "degreeProgram" for the degree programs --
+* containing data types: SECURITY, NETWORK, SOFTWARE 
+-----uhh so i added, but do i need to do anything more?? idk*/
+
+
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
+
+//static const string degreeString[] = { "SECURITY", "NETWORK", "SOFTWARE" }; //i dont htink i need this? idk what its even for i dont remember why i added it
